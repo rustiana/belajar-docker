0f62a3a41d0073a302d6a661abb86d364a1d8596
Deden Rustiana <rustianadeden25@gmail.com>
2022-08-17 20:30:34 +0700

    dockerfile update

--- script.sh
+++ script.sh
@@ -11,3 +11,24 @@ docker image rm denrus25/run
 docker container create --name container_run denrus25/run:latest
 docker container start container_run
 docker container rm container_run
+
+--build dockerfile command
+docker build -t denrus25/cmd cmd --progress=plain
+
+docker image inspect denrus25/cmd
+
+docker container create --name container_cmd denrus25/cmd
+docker container start container_cmd
+docker container logs container_cmd
+
+--build dockerfile label
+docker build -t denrus25/label label --progress=plain
+
+docker image inspect denrus25/label
+
+--build dockerfile add instruction
+docker build -t denrus25/add add --progress=plain
+
+docker container create --name container_add denrus25/add
+docker container start container_add
+docker container logs container_add
